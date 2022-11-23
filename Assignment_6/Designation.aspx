afe1b28a4a858447b746e836c42ef5c8096865cf
Anurag <Anurag@NTP-LAP-841>
2022-11-23 23:28:38 +0530

    Add project files.

---
+++ Assignment_6/Designation.aspx
@@ -0,0 +1,54 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Designation.aspx.cs" Inherits="Assignment_6.Designation" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body style="background-color:antiquewhite">
+    <form id="form1" runat="server">
+        <div>
+            <h2 align="center">Employee Registration</h2>
+            <table align="center">
+                <tr>
+                    <td>
+                        Designation :
+                    </td>
+                    <td> <asp:TextBox ID="textdesg" runat="server"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                <td>Department :
+                </td>
+            <td><asp:DropDownList ID="DropDownList1" runat="server"></asp:DropDownList> </td>
+          </tr>  
+                <tr>
+                    <td colspan="2" align="center"><asp:Button ID="Button1" runat="server" Text="Submit" OnClick="Button1_Click"/></td>
+                </tr>
+            </table>
+        </div>
+         <table align="center">
+                <tr>
+                    <td>&nbsp</td>
+                    <td>&nbsp</td>
+                </tr>
+                <tr>
+                    <td>
+                        <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="False" DataKeyNames="DesignationId" OnRowEditing="Gridview1_RowEditing" OnRowUpdating="Gridview1_RowUpdating" OnRowCancelingEdit="Gridview1_RowCancelingEdit" OnRowDeleting="Gridview1_RowDeleting">
+                            <Columns>
+            
+                                <asp:BoundField DataField="DesignationId" HeaderText="Designation ID" />                 
+                                <asp:BoundField DataField="DesignationName" HeaderText="Designation Name" />
+                                <asp:BoundField DataField="DepartmentId" HeaderText="Department_Id" />
+                                <asp:CommandField HeaderText="Edit" ShowEditButton="true" />
+                                <asp:CommandField HeaderText="Delete" ShowDeleteButton="true" />
+                               
+                            </Columns>
+                        </asp:GridView>
+                    </td>
+                </tr>
+        </table>
+    </form>
+</body>
+</html>
---
+++ Assignment_6/Designation.aspx.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Assignment_6
+{
+    public partial class Designation : System.Web.UI.Page
+    {
+        DBOperation db = new DBOperation();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack) //can avoid page refreshing problem
+            {
+                SqlConnection con1 = new SqlConnection();
+                con1.ConnectionString = "Data Source=NTP-LAP-841;Initial Catalog=Assignments;Integrated Security=True;Pooling=False";
+                con1.Open();
+                /*SqlCommand com = new SqlCommand("Select * from Department", con1);
+                SqlDataAdapter da = new SqlDataAdapter(com);
+                DataSet ds = new DataSet();
+                da.Fill(ds);*/
+                DropDownList1.DataSource = db.exedataset("Select * from dep");
+                DropDownList1.DataTextField = "depname";
+                DropDownList1.DataValueField = "DepartmentId";
+                DropDownList1.DataBind();
+                GridView1.DataSource = db.exedataset("select * from Designation");
+                GridView1.DataBind();
+
+            }
+
+        }
+        protected void Gridview1_RowEditing(object sender, GridViewEditEventArgs e)
+        {
+            GridView1.EditIndex = e.NewEditIndex; //set edit index
+            GridView1.DataSource = db.exedataset("select * from Designation"); //value taking from table
+            GridView1.DataBind(); //binding data extracted from the table to the grid view 
+        }
+
+        protected void Gridview1_RowUpdating(object sender, GridViewUpdateEventArgs e)
+        {
+            //take datakey value from grid view
+            string Designation_Id = GridView1.DataKeys[e.RowIndex].Value.ToString(); //tacking emp value of the selected row
+            //create a text box and initialize
+            TextBox txtDesg = new TextBox();
+            //access first column and assign to textbox
+            txtDesg = (TextBox)GridView1.Rows[e.RowIndex].Cells[1].Controls[0];
+            Response.Write("<script>alert(txtDesg);</script>");
+            db.exenonquery("update Designation set DesignationName='" + txtDesg.Text + "'where DesignationId='" + Designation_Id + "'");
+            GridView1.EditIndex = -1;  //previous view stage -1 indicate previous view only 
+            GridView1.DataSource = db.exedataset("select * from Designation");
+            GridView1.DataBind();
+
+        }
+
+        protected void Gridview1_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
+        {
+            GridView1.EditIndex = -1;  //previous view stage -1 indicate previous view only 
+            GridView1.DataSource = db.exedataset("select * from Designation");
+            GridView1.DataBind();
+
+        }
+
+        protected void Gridview1_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            string desg_id = GridView1.DataKeys[e.RowIndex].Value.ToString(); //tacking emp value of the selected row
+            db.exenonquery("Delete from Designation where DesignationId='" + desg_id + "'");
+            GridView1.DataSource = db.exedataset("select * from Designation");
+            GridView1.DataBind();
+        }
+
+        protected void GridView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            int id = int.Parse(DropDownList1.SelectedItem.Value);
+            string sql = "insert into Designation values('" + textdesg.Text + "','" + id + "')";
+            int i = db.exenonquery(sql);
+            if (i == 1)
+            {
+                Response.Write("<script>alert('inserted Successfully ');</script>");
+            }
+            else
+            {
+                Response.Write("Insertion Fail");
+            }
+            GridView1.DataSource = db.exedataset("select * from Designation");
+            GridView1.DataBind();
+        }
+    }
+}
